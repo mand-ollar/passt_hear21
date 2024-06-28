146b6c8a08195856f194b6ae88da0ecfc7fb08b3
Minseok Oh <108140450+mand-ollar@users.noreply.github.com>
2024-06-28 14:57:02 +0900

    remove unsqueeze

--- hear21passt/models/preprocess.py
+++ hear21passt/models/preprocess.py
@@ -56,7 +56,6 @@ class AugmentMelSTFT(nn.Module):
         x = torch.stft(x, self.n_fft, hop_length=self.hopsize, win_length=self.win_length,
                        center=True, normalized=False, window=self.window, return_complex=True)
         x = torch.view_as_real(x)
-        x = x.unsqueeze(dim=-1)
         x = (x ** 2).sum(dim=-1)  # power mag
         fmin = self.fmin + torch.randint(self.fmin_aug_range, (1,)).item()
         fmax = self.fmax + self.fmax_aug_range // 2 - torch.randint(self.fmax_aug_range, (1,)).item()
